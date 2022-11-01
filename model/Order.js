896837ef2726cbecdbf73dc07d27cbb958a3910d
mengiefen <mengiefen@gmail.com>
2022-11-01 22:30:24 +0300

    add Order schema

---
+++ model/Order.js
@@ -0,0 +1,28 @@
+const cuid = require("cuid");
+const { emailSchema } = require("./validations");
+const db = require("./db");
+
+const OrderSchema = new db.Schema({
+  _id: { type: String, default: cuid },
+  buyerEmail: emailSchema({ required: true }),
+  products: [
+    
+    {
+      type: String,
+      ref: "Product",
+      index: true,
+      required: true,
+    },
+  ],
+
+  status: {
+    type: String,
+    index: true,
+    default: "CREATED",
+    enum: ["CREATED", "PAID", "SHIPPED", "DELIVERED", "CANCELLED"],
+  },
+});
+
+const Order = db.model("Order", OrderSchema);
+
+module.exports = Order;
