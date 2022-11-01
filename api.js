6027b395fc5fe8f3deecac96920d1d762e213406
mengiefen <mengiefen@gmail.com>
2022-11-01 22:28:38 +0300

    add api to connect to database

---
+++ api.js
@@ -0,0 +1,67 @@
+const Products = require('./routes/products');
+const Orders = require('./routes/orders');
+const { autoCatch } = require('./lib/auto-catch'); // callback function for /products?offset=0&limit=25
+
+async function listProducts(req, res) {
+  const { offset = 0, limit = 25, tag } = req.query;
+  const products = await Products.list({
+    offset: Number(offset),
+    limit: Number(limit),
+    tag,
+  });
+
+  res.json(products);
+}
+
+// callback function for /products/:id
+async function getProduct(req, res, next) {
+  const { id } = req.params;
+
+  const product = await Products.get(id);
+  if (!product) return next();
+
+  res.json(product);
+}
+
+async function createProducts(req, res, next) {
+  const product = await Products.create(req.body);
+  res.json(product);
+}
+
+async function updateProduct(req, res, next) {
+  const product = await Products.update(req.params.id, req.body);
+  res.json(product);
+}
+
+async function deleteProduct(req, res, next) {
+  await Products.delete(req.params.id);
+  res.json({ success: true, message: 'Product deleted' });
+}
+
+async function listOrders(req, res, next) {
+  const { offset = 0, limit = 25, productId, status } = req.query;
+  const orders = await Orders.list({
+    offset: Number(offset),
+    limit: Number(limit),
+    productId,
+    status,
+  });
+
+  res.json(orders);
+}
+
+async function createOrder(req, res, next) {
+  const order = await Orders.create(req.body);
+  res.json(order);
+}
+
+module.exports = autoCatch({
+  listProducts,
+  getProduct,
+  createProducts,
+  updateProduct,
+  deleteProduct,
+
+  listOrders,
+  createOrder,
+});
