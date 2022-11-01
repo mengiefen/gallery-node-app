80d945a4b5b704bfc501d17e9c482295d4b59469
mengiefen <mengiefen@gmail.com>
2022-11-01 22:30:33 +0300

    add  Product schema

---
+++ model/Product.js
@@ -0,0 +1,19 @@
+const cuid = require("cuid");
+const db = require("./db");
+const { urlSchema } = require("./validations");
+
+const ProductSchema = new db.Schema({
+  _id: { type: String, default: cuid },
+  description: { type: String, required: true },
+  imgThumb: urlSchema({ required: true }),
+  img: urlSchema({ required: true }),
+  link: urlSchema(),
+  userId: { type: String, required: true },
+  userName: { type: String, required: true },
+  userLink: urlSchema(),
+  tags: { type: [String], index: true },
+});
+
+const Product = db.model("Product", ProductSchema);
+
+module.exports = Product;
