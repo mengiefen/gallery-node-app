e326ec19ed9c2fb913101f76638bdaacc89adf04
mengiefen <mengiefen@gmail.com>
2022-11-03 16:44:11 +0300

    add authorization for protected routes

--- api.js
+++ api.js
@@ -25,34 +25,42 @@ async function getProduct(req, res, next) {
 }
 
 async function createProducts(req, res, next) {
+  if (!req.isAdmin) return forbidden(next);
   const product = await Products.create(req.body);
   res.json(product);
 }
 
 async function updateProduct(req, res, next) {
+  if (!req.isAdmin) return forbidden(next);
+
   const product = await Products.update(req.params.id, req.body);
   res.json(product);
 }
 
 async function deleteProduct(req, res, next) {
+  if (!req.isAdmin) return forbidden(next);
+
   await Products.delete(req.params.id);
   res.json({ success: true, message: "Product deleted" });
 }
 
 async function listOrders(req, res, next) {
   const { offset = 0, limit = 25, productId, status } = req.query;
-  const orders = await Orders.list({
+  const options = {
     offset: Number(offset),
     limit: Number(limit),
     productId,
     status,
-  });
-
+  };
+  if (!req.isAdmin) options.username = req.user.username;
+  const orders = await Orders.list(options);
   res.json(orders);
 }
 
 async function createOrder(req, res, next) {
-  const order = await Orders.create(req.body);
+  const fields = req.body;
+  if (!req.isAdmin) fields.username = req.user.username;
+  const order = await Orders.create(fields);
   res.json(order);
 }
 
@@ -91,6 +99,12 @@ async function deleteUser(req, res, next) {
   const { id } = req.params;
   await Users.delete(id);
   res.json({ success: true, message: "User deleted" });
+}
+
+function forbidden(next) {
+  const err = new Error("Forbidden");
+  err.status = 403;
+  return next(err);
 }
 
 module.exports = autoCatch({
